533289aae7e0bdb8c0c6f338dab3c699045779b2
Jack Hermanson <jack.hermanson@live.com>
2021-07-28 14:28:21 -0600

    show basic inputs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,40 @@ import React from "react";
 
 function App() {
     return (
-        <div>
-            <p>this is the app</p>
+        <div
+            style={{
+                padding: "2em",
+            }}
+        >
+            {renderInput()}
+            <br />
+            {renderOutput()}
         </div>
     );
+
+    function renderInput() {
+        const id = "input";
+        return (
+            <div>
+                <label style={{ paddingRight: "1em" }} htmlFor={id}>
+                    Input
+                </label>
+                <input type="text" id={id} />
+            </div>
+        );
+    }
+
+    function renderOutput() {
+        const id = "output";
+        return (
+            <div>
+                <label style={{ paddingRight: "1em" }} htmlFor={id}>
+                    Output
+                </label>
+                <input type="text" id={id} />
+            </div>
+        );
+    }
 }
 
 export default App;
