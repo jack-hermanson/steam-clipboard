38c55cde222bde8bb070d3df3144c65c98f4d3fa
Jack Hermanson <jack.hermanson@live.com>
2021-07-28 14:57:00 -0600

    does not work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 function App() {
+    const [input, setInput] = useState<string>("");
+    const [output, setOutput] = useState<string>("");
+
+    const urlPrefix = "steam://openurl/";
+
+    const clipboardAction = useCallback(async () => {
+        const clipboardText = await navigator.clipboard.readText();
+        setInput(clipboardText);
+        const newText = `${urlPrefix}${clipboardText}`;
+        setOutput(newText);
+    }, [setOutput, urlPrefix]);
+
+    useEffect(() => {
+        if (output === "" && input === "") {
+            console.log("ok");
+            clipboardAction().then(() => {
+                const outputElement = document.getElementById(
+                    "output"
+                ) as HTMLInputElement;
+                outputElement.select();
+                document.execCommand("copy");
+            });
+        }
+    }, [clipboardAction, output, input]);
+
     return (
         <div
             style={{
@@ -20,7 +45,12 @@ function App() {
                 <label style={{ paddingRight: "1em" }} htmlFor={id}>
                     Input
                 </label>
-                <input type="text" id={id} />
+                <input
+                    onChange={e => setInput(e.target.value)}
+                    value={input}
+                    type="text"
+                    id={id}
+                />
             </div>
         );
     }
@@ -32,7 +62,12 @@ function App() {
                 <label style={{ paddingRight: "1em" }} htmlFor={id}>
                     Output
                 </label>
-                <input type="text" id={id} />
+                <input
+                    onChange={e => setOutput(e.target.value)}
+                    value={output}
+                    type="text"
+                    id={id}
+                />
             </div>
         );
     }
